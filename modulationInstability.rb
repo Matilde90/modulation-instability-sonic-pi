042b0290d1296dc74362f43831f303c146974ed7
Matilde90 <matilde.aliffi@gmail.com>
2020-04-26 23:29:06 +0100

    refactoring function

--- modulationInstability.rb
+++ modulationInstability.rb
@@ -1,27 +1,25 @@
 require 'csv'
-local_dir="/Users/matildealiffi/Coding/music_sonic_pi/modulation-instability-sonic-pi/"
-amp_table=CSV.parse(File.read(local_dir + "ex5.csv"), headers: false)
-freq_table=CSV.parse(File.read(local_dir + "freq5.csv"), headers: false)
+LOCAL_DIR="/Users/matildealiffi/Coding/music_sonic_pi/modulation-instability-sonic-pi/"
+AMP_TABLE=CSV.parse(File.read(LOCAL_DIR + "ex5.csv"), headers: false)
+FREQ_TABLE=CSV.parse(File.read(LOCAL_DIR + "freq5.csv"), headers: false)
+FILTER = 0.009
 
-filter = 0.009;
-puts "the script will cut off frequences with amplitude lower than #{filter}.";
+puts "the script will cut off frequences with amplitude lower than #{FILTER}."
 
-column_in_amp = amp_table.transpose[0].size - 1;
-array = [];
+column_in_amp = AMP_TABLE.transpose[0].size - 1
+master_chords_array = []
 
 column_in_amp.times do |i|
-    accordo = [];
-    amp_table[i].length.times do |n|;
-      if amp_table[i][n].to_f > filter;
-            accordo.push([freq_table[0][n].to_f,amp_table[i][n].to_f]); # [[freq, amp], [freq, amp] ...]
+    accordo = []
+    AMP_TABLE[i].length.times do |n|
+      if AMP_TABLE[i][n].to_f > FILTER
+            accordo.push([FREQ_TABLE[0][n].to_f,AMP_TABLE[i][n].to_f]); # [[freq, amp], [freq, amp] ...]
       end
     end
-    if accordo.length > 10;
-        sorted_array=accordo.sort {|a, b| b[1] <=> a[1]} # [[79.6, 0.99], [60.8, 0.98 ], [90,0.97],[79.6, 0.9], [60.8, 0.8 ], [90,0.77]]
-        cutted_array = sorted_array.first(10)
-        array.push(cutted_array) # [[79.6, 0.9], [60.8, 0.8 ], [90,0.77]]
-    else array.push(accordo) if accordo.length > 0
-             # accordo: [[79.6, 0.8], [60.8, 0.9 ], [90,0.77]]
+    if accordo.length > 10 # [[79.6, 0.99], [60.8, 0.98 ], [90,0.97],[79.6, 0.9], [60.8, 0.8 ], [90,0.77]]
+        cutted_master_chords_array = accordo.sort {|a, b| b[1] <=> a[1]}.first(10)
+        master_chords_array.push(cutted_master_chords_array) # [[79.6, 0.9], [60.8, 0.8 ], [90,0.77]]
+    else master_chords_array.push(accordo) if accordo.length > 0 # accordo: [[79.6, 0.8], [60.8, 0.9 ], [90,0.77]]
     end
 end
 
